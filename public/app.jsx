94cbe59fb4fb3592b3a4b72a0d3a083be5f5b23e
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-07-24 12:01:01 -0700

    Pass prop to component

--- public/app.jsx
+++ public/app.jsx
@@ -1,8 +1,9 @@
 var Greeter = React.createClass({
     render: function () {
+        var name = this.props.name;
         return (
             <div>
-                <h1>Hello React!</h1>
+                <h1>Hello { name }!</h1>
                 <p>This is from a React component!</p>
             </div>
         );
@@ -10,6 +11,6 @@ var Greeter = React.createClass({
 });
 
 ReactDOM.render(
-    <Greeter/>,
+    <Greeter name="Tso-Liang"/>,
     document.getElementById('app')
 );
